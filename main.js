cfe9696cc5bed880c45aa3ce72fc59e071fa25f2
rubengg33 <135147356+rubengg33@users.noreply.github.com>
2024-10-29 12:51:30 +0100

    cambios importantes aunque no parezca, menu desplegable

--- main.js
+++ main.js
@@ -42,46 +42,126 @@ class RelativeTime extends HTMLElement {
 customElements.define('relative-time', RelativeTime);
 const articles = [
     {
-        "id": 1,
-        "image": "https://cdn.kqed.org/wp-content/uploads/sites/35/2024/10/GettyImages-1347890261-1020x680.jpg",
-        "title": "Climate Activists Push for Carbon Tax With Measure GG, But Critics Warn it Could Backfire",
-        "description": "Grassroots climate activists argue a tax on gas use in large buildings will help all of Berkeley kick fossil fuels. But many politicians, businesses, and nonprofits, even those that work on climate, don’t think Measure GG is the policy to get there.",
-        "date": "2023-10-17T11:00:16Z",
-        "category": "Climate"
+      "id": 1,
+      "image": "https://cdn.kqed.org/wp-content/uploads/sites/35/2024/10/GettyImages-1347890261-1020x680.jpg",
+      "title": "Climate Activists Push for Carbon Tax With Measure GG, But Critics Warn it Could Backfire",
+      "description": "Grassroots climate activists argue a tax on gas use in large buildings will help all of Berkeley kick fossil fuels. But many politicians, businesses, and nonprofits, even those that work on climate, don’t think Measure GG is the policy to get there.",
+      "date": "2023-10-17T11:00:16Z",
+      "category": "Climate"
     },
     {
-        "id": 2,
-        "image": "https://fortune.com/img-assets/wp-content/uploads/2024/10/GettyImages-2170862982_413c33-e1729160290535.jpg?resize=1200,600",
-        "title": "Musk’s empire risks being targeted by EU for potential X fines",
-        "description": "The EU may target Elon Musk’s broader business empire for X fines, potentially including revenue from SpaceX and Neuralink to increase penalties.",
-        "date": "2024-09-17T10:32:45Z",
-        "category": "Business"
+      "id": 2,
+      "image": "https://fortune.com/img-assets/wp-content/uploads/2024/10/GettyImages-2170862982_413c33-e1729160290535.jpg?resize=1200,600",
+      "title": "Musk’s empire risks being targeted by EU for potential X fines",
+      "description": "The EU may target Elon Musk’s broader business empire for X fines, potentially including revenue from SpaceX and Neuralink to increase penalties.",
+      "date": "2024-09-17T10:32:45Z",
+      "category": "Business"
     },
     {
-        "id": 3,
-        "image": "https://media.autoexpress.co.uk/image/private/s--X-WVjvBW--/f_auto,t_content-image-full-desktop@1/v1729160267/autoexpress/2024/10/Omode E5 first UK drive.jpg",
-        "title": "Omoda E5 in Noble trim - pictures",
-        "description": "Pictures of the electric Omode E5 SUV being driven on UK roads. Pictures taken by Auto Express senior photographer Pete Gibson",
-        "date": "2024-10-15T10:30:56Z",
-        "category": "Automobile"
+      "id": 3,
+      "image": "https://media.autoexpress.co.uk/image/private/s--X-WVjvBW--/f_auto,t_content-image-full-desktop@1/v1729160267/autoexpress/2024/10/Omode E5 first UK drive.jpg",
+      "title": "Omoda E5 in Noble trim - pictures",
+      "description": "Pictures of the electric Omode E5 SUV being driven on UK roads. Pictures taken by Auto Express senior photographer Pete Gibson",
+      "date": "2024-10-15T10:30:56Z",
+      "category": "Automobile"
     },
     {
-        "id": 4,
-        "image": "https://cdn.mos.cms.futurecdn.net/6xqynicLzH6sSskfiNyWoT-1200-80.jpg",
-        "title": "Renault, Alpine and Citroën help the 2024 Paris Motor Show return to form",
-        "description": "Explore the most delectable debuts at the Paris Motor Show 2024 – or Mondial de l'Auto – including designs from France's big car makers and niche machines from around the world",
-        "date": "2024-10-13T10:25:24Z",
-        "category": "Automobile"
+      "id": 4,
+      "image": "https://cdn.mos.cms.futurecdn.net/6xqynicLzH6sSskfiNyWoT-1200-80.jpg",
+      "title": "Renault, Alpine and Citroën help the 2024 Paris Motor Show return to form",
+      "description": "Explore the most delectable debuts at the Paris Motor Show 2024 – or Mondial de l'Auto – including designs from France's big car makers and niche machines from around the world",
+      "date": "2024-10-13T10:25:24Z",
+      "category": "Automobile"
     },
     {
-        "id": 5,
-        "image": "https://www.computerworld.com/wp-content/uploads/2024/10/3567767-0-90640600-1729160617-IDG-Germany-Intel-September-News.jpg?quality=50&strip=all&w=1024",
-        "title": "Chinese cybersecurity association urges review of Intel products",
-        "description": "The Cybersecurity Association of China (CSAC) has urged a security review of Intel products sold in the country, claiming the US semiconductor firm poses ongoing threats to China’s national security and interests.",
-        "date": "2024-10-01T10:22:56Z",
-        "category": "Technology"
+      "id": 5,
+      "image": "https://www.computerworld.com/wp-content/uploads/2024/10/3567767-0-90640600-1729160617-IDG-Germany-Intel-September-News.jpg?quality=50&strip=all&w=1024",
+      "title": "Chinese cybersecurity association urges review of Intel products",
+      "description": "The Cybersecurity Association of China (CSAC) has urged a security review of Intel products sold in the country, claiming the US semiconductor firm poses ongoing threats to China’s national security and interests.",
+      "date": "2024-10-01T10:22:56Z",
+      "category": "Technology"
+    },
+    {
+      "id": 6,
+      "image": "https://cdn.kqed.org/wp-content/uploads/sites/35/2024/10/GettyImages-1347890261-1020x680.jpg",
+      "title": "Climate Activists Push for Carbon Tax With Measure GG, But Critics Warn it Could Backfire",
+      "description": "Grassroots climate activists argue a tax on gas use in large buildings will help all of Berkeley kick fossil fuels. But many politicians, businesses, and nonprofits, even those that work on climate, don’t think Measure GG is the policy to get there.",
+      "date": "2023-10-17T11:00:16Z",
+      "category": "Climate"
+    },
+    {
+      "id": 7,
+      "image": "https://fortune.com/img-assets/wp-content/uploads/2024/10/GettyImages-2170862982_413c33-e1729160290535.jpg?resize=1200,600",
+      "title": "Musk’s empire risks being targeted by EU for potential X fines",
+      "description": "The EU may target Elon Musk’s broader business empire for X fines, potentially including revenue from SpaceX and Neuralink to increase penalties.",
+      "date": "2024-09-17T10:32:45Z",
+      "category": "Business"
+    },
+    {
+      "id": 8,
+      "image": "https://media.autoexpress.co.uk/image/private/s--X-WVjvBW--/f_auto,t_content-image-full-desktop@1/v1729160267/autoexpress/2024/10/Omode E5 first UK drive.jpg",
+      "title": "Omoda E5 in Noble trim - pictures",
+      "description": "Pictures of the electric Omode E5 SUV being driven on UK roads. Pictures taken by Auto Express senior photographer Pete Gibson",
+      "date": "2024-10-15T10:30:56Z",
+      "category": "Automobile"
+    },
+    {
+      "id": 9,
+      "image": "https://cdn.mos.cms.futurecdn.net/6xqynicLzH6sSskfiNyWoT-1200-80.jpg",
+      "title": "Renault, Alpine and Citroën help the 2024 Paris Motor Show return to form",
+      "description": "Explore the most delectable debuts at the Paris Motor Show 2024 – or Mondial de l'Auto – including designs from France's big car makers and niche machines from around the world",
+      "date": "2024-10-13T10:25:24Z",
+      "category": "Automobile"
+    },
+    {
+      "id": 10,
+      "image": "https://www.computerworld.com/wp-content/uploads/2024/10/3567767-0-90640600-1729160617-IDG-Germany-Intel-September-News.jpg?quality=50&strip=all&w=1024",
+      "title": "Chinese cybersecurity association urges review of Intel products",
+      "description": "The Cybersecurity Association of China (CSAC) has urged a security review of Intel products sold in the country, claiming the US semiconductor firm poses ongoing threats to China’s national security and interests.",
+      "date": "2024-10-01T10:22:56Z",
+      "category": "Technology"
+    },
+    {
+      "id": 11,
+      "image": "https://cdn.kqed.org/wp-content/uploads/sites/35/2024/10/GettyImages-1347890261-1020x680.jpg",
+      "title": "Climate Activists Push for Carbon Tax With Measure GG, But Critics Warn it Could Backfire",
+      "description": "Grassroots climate activists argue a tax on gas use in large buildings will help all of Berkeley kick fossil fuels. But many politicians, businesses, and nonprofits, even those that work on climate, don’t think Measure GG is the policy to get there.",
+      "date": "2023-10-17T11:00:16Z",
+      "category": "Climate"
+    },
+    {
+      "id": 12,
+      "image": "https://fortune.com/img-assets/wp-content/uploads/2024/10/GettyImages-2170862982_413c33-e1729160290535.jpg?resize=1200,600",
+      "title": "Musk’s empire risks being targeted by EU for potential X fines",
+      "description": "The EU may target Elon Musk’s broader business empire for X fines, potentially including revenue from SpaceX and Neuralink to increase penalties.",
+      "date": "2024-09-17T10:32:45Z",
+      "category": "Business"
+    },
+    {
+      "id": 13,
+      "image": "https://media.autoexpress.co.uk/image/private/s--X-WVjvBW--/f_auto,t_content-image-full-desktop@1/v1729160267/autoexpress/2024/10/Omode E5 first UK drive.jpg",
+      "title": "Omoda E5 in Noble trim - pictures",
+      "description": "Pictures of the electric Omode E5 SUV being driven on UK roads. Pictures taken by Auto Express senior photographer Pete Gibson",
+      "date": "2024-10-15T10:30:56Z",
+      "category": "Automobile"
+    },
+    {
+      "id": 14,
+      "image": "https://cdn.mos.cms.futurecdn.net/6xqynicLzH6sSskfiNyWoT-1200-80.jpg",
+      "title": "Renault, Alpine and Citroën help the 2024 Paris Motor Show return to form",
+      "description": "Explore the most delectable debuts at the Paris Motor Show 2024 – or Mondial de l'Auto – including designs from France's big car makers and niche machines from around the world",
+      "date": "2024-10-13T10:25:24Z",
+      "category": "Automobile"
+    },
+    {
+      "id": 15,
+      "image": "https://www.computerworld.com/wp-content/uploads/2024/10/3567767-0-90640600-1729160617-IDG-Germany-Intel-September-News.jpg?quality=50&strip=all&w=1024",
+      "title": "Chinese cybersecurity association urges review of Intel products",
+      "description": "The Cybersecurity Association of China (CSAC) has urged a security review of Intel products sold in the country, claiming the US semiconductor firm poses ongoing threats to China’s national security and interests.",
+      "date": "2024-10-01T10:22:56Z",
+      "category": "Technology"
     }
-]
+  ]
 
 class CustomSearch extends HTMLElement {
     constructor() {
@@ -195,7 +275,11 @@ const navbar = document.querySelector('.navbar');
 const abrirMenu = document.querySelector('.menu-toggle');
 const cerrarMenu = document.querySelector('.menu-close');
 abrirMenu.addEventListener('click', () => {
+    navbar.classList.add('visible');
 })
 cerrarMenu.addEventListener('click', () => {
-    nav.classList.remove('visible');
+    navbar.classList.remove('visible');
+    // .menu-toggle{
+    //     display: none;
+    //   }
 })
